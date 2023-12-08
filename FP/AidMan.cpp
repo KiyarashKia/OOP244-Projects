c76728523a82ef2fbfb9796c749fd2a2a0b04a2f
Kiyarash Kia <kiarashkia1999@gmail.com>
2023-12-08 00:28:48 -0500

    Fixing Typo in Aidman.cpp removeItem

--- FP/AidMan.cpp
+++ FP/AidMan.cpp
@@ -304,7 +304,7 @@ namespace sdds {
             int sku = ut.getint("Enter SKU: ");
             int index = search(sku);
             if (index >= 0) {
-                cout << "Following item will be removed:" << endl;
+                cout << "Following item will be removed: " << endl;
                 m_items[index]->linear(false);
                 m_items[index]->display(cout) << endl;
                 cout << "Are you sure?" << endl;
